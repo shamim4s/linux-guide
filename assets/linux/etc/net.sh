ddfe4f9e8b73ad5a3ef3edcedc24287fdd58ba95
shamim4s <shamim4s@gmail.com>
2021-07-24 03:15:00 +0600

    sh file changed

--- assets/linux/etc/net.sh
+++ assets/linux/etc/net.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 clear
+echo  "GRUB_GFXPAYLOAD_LINUX=800x600" >> "/etc/default/grub"
+update-grub
+clear
+echo  " Resizing Disk for the first time"
+parted ---pretend-input-tty <<EOD
+resizepart
+fix
+1
+Yes
+100%
+quit
+EOD
+resize2fs /dev/sda1
+ssh-keygen -A
+systemctl restart ssh
+clear
 echo ""
 echo "----------------------------------------------------------"
 echo " Welcome To Ubuntu Cloud Official "
@@ -11,16 +27,15 @@ echo "Connection for the first time"
 sleep 0.5
 echo "it will take few seconds "
 sleep 0.5
-echo ""
+rm /etc/netplan/*.yaml
+sleep 0.5
 network_name_all=$(ip -o link show | awk -F': ' '{print $2}')
 echo "Your all network interface name "$network_name_all
-echo ""
 sleep 0.5
 network_name=$(echo $network_name_all  | awk -- '{print $2}')
-echo ""
 echo "Your main network interface name "$network_name
 sleep 0.5
-echo "configuring your network for " $network_name
+echo "First time configuring your network for " $network_name
 echo  "network:" >> "/etc/netplan/"$network_name".yaml"
 echo  "  version: 2" >> "/etc/netplan/"$network_name".yaml"
 echo  "  renderer: networkd" >> "/etc/netplan/"$network_name".yaml"
@@ -29,10 +44,10 @@ echo  "   "$network_name":" >> "/etc/netplan/"$network_name".yaml"
 echo  "     dhcp6: true" >> "/etc/netplan/"$network_name".yaml"
 echo  "     dhcp4: true" >> "/etc/netplan/"$network_name".yaml"
 netplan apply
-echo ""
-echo ""
+echo  "Network Configuration Completed"
 echo "Finish"
 echo "Now you can start working"
+echo "You can change your password by command 'passwd root'"
 echo "Thank you"
 rm /etc/profile.d/net.sh
 echo "----------------------------------------------------------"
